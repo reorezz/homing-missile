1194f84cd856ac917642a2b0cf6913ad04b4c862
reorezz <46937649+reorezz@users.noreply.github.com>
2021-03-20 02:10:50 -0700

    Create TutorialScript.cs

---
+++ TutorialScript.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+
+    public Transform targetTransform;
+    public float speeddOfMissile = 1.5f;
+
+    private void Update()
+    {
+        //rotaion
+        Vector2 tempDir = targetTransform.position - transform.position; //direction vector is calculated 
+
+        float newAngle = Vector2.SignedAngle(Vector2.up, tempDir); //angle between direction vector and +ve y axis is calculated
+
+        transform.rotation = Quaternion.Euler(0f, 0f, newAngle); //angle is given to missile so that it points towards the target 
+
+
+
+        //move
+        transform.Translate(Vector2.up * Time.deltaTime * speeddOfMissile);//we move the missile in the direction in which the missile is looking
+
+
+
+
+        //please  kill me 
+
+
+    }
+
+}
